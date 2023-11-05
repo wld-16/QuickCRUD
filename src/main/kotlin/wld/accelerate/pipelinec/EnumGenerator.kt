e104ef3a23202466f08abe7ffb31efd6ff2d3782
wld <w.nabok@gmail.com>
2023-11-05 23:48:21 +0100

    Enum generator actions

---
+++ src/main/kotlin/wld/accelerate/pipelinec/EnumGenerator.kt
@@ -0,0 +1,44 @@
+package wld.accelerate.pipelinec
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import org.jetbrains.annotations.NotNull
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+
+
+class EnumGenerator : AnAction() {
+    override fun update(@NotNull event: AnActionEvent) {
+        // Using the event, evaluate the context,
+        // and enable or disable the action.
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        val yaml: Yaml = Yaml()
+
+        return super.getActionUpdateThread()
+    }
+
+    override fun actionPerformed(@NotNull event: AnActionEvent) {
+        FileChooser.chooseFile(FileChooserDescriptorFactory.createSingleFileDescriptor(), event.project, null) {
+                val file = it.canonicalFile
+
+                val yamlMap = parseYaml(file!!.path)
+
+                val linkedHashMap = yamlMap?.get("enums") as LinkedHashMap<String, String>
+
+                val enumClassRepresentations = writeJavaEnums(linkedHashMap, yamlMap["packagePath"] as String)
+
+                enumClassRepresentations.forEach { fileEntry ->
+                    File(event.project!!.basePath + "/src/" + fileEntry.key + ".java").writeText(fileEntry.value)
+                }
+
+        }
+
+        // Using the event, implement an action.
+        // For example, create and show a dialog.
+    } // Override getActionUpdateThread() when you target 2022.3 or later!
+}
--- src/test/kotlin/wld/accelerate/pipelinec/YamlGenerateKotlinTest.kt
+++ src/test/kotlin/wld/accelerate/pipelinec/YamlGenerateKotlinTest.kt
@@ -7,7 +7,7 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 
-class YamlKtTest() {
+class YamlGenerateKotlinTest() {
     @Test
     fun test_parseYaml() {
         val resourceName = "sample/example.yaml"
@@ -38,44 +38,6 @@ class YamlKtTest() {
         }
     }
 
-    @Test
-    fun test_writeJavaEnums() {
-        val resourceName = "sample/example.yaml"
-
-        val classLoader = javaClass.classLoader
-        val file = File(classLoader.getResource(resourceName)!!.file)
-
-        val yamlMap = parseYaml(file.absolutePath)
-
-        val linkedHashMap = yamlMap?.get("enums") as LinkedHashMap<String, String>
-
-        val enumClassRepresentations = writeJavaEnums(linkedHashMap, yamlMap["packagePath"] as String)
-
-        enumClassRepresentations.forEach{
-            File("src/test/kotlin/wld/accelerate/pipelinec/java/" + it.key +".java").writeText(it.value)
-        }
-    }
-
-    @Test
-    fun test_writeJavaControllers() {
-        test_writeJavaRepositories()
-
-        val resourceName = "sample/example.yaml"
-
-        val classLoader = javaClass.classLoader
-        val file = File(classLoader.getResource(resourceName)!!.file)
-
-        val yamlMap = parseYaml(file.absolutePath)
-
-        writeJavaControllerClasses(listOf(), (yamlMap?.get("packagePath") as String) + ".controller")
-
-        val entityClassRepresentations = writeJavaControllerClasses((yamlMap["entities"] as Map<String, *>).keys.toList(), (yamlMap["packagePath"] as String) + ".java.controller")
-
-        entityClassRepresentations.forEach{
-            File("src/test/kotlin/wld/accelerate/pipelinec/java/controller/" + it.key + "Controller.java").writeText(it.value)
-        }
-    }
-
     /*
     @Test
     fun test_writeKotlinControllers() {
@@ -96,23 +58,6 @@ class YamlKtTest() {
     }
     */
 
-    @Test
-    fun test_writeJavaRepositories() {
-        test_writeEntityDataClass_java()
-
-        val resourceName = "sample/example.yaml"
-
-        val classLoader = javaClass.classLoader
-        val file = File(classLoader.getResource(resourceName)!!.file)
-
-        val yamlMap = parseYaml(file.absolutePath)
-
-        val entityClassRepresentations = writeJavaRepositoryDataClass((yamlMap?.get("entities") as Map<String, Map<String, Any>>).keys.toList(), (yamlMap["packagePath"] as String) + ".java.repository")
-
-        entityClassRepresentations.forEach{
-            File("src/test/kotlin/wld/accelerate/pipelinec/java/repository/" + it.key + "Repository.java").writeText(it.value)
-        }
-    }
 
     @Test
     fun test_writeRepositories_kotlin() {
@@ -129,24 +74,6 @@ class YamlKtTest() {
 
         entityClassRepresentations.forEach{
             File("src/test/kotlin/wld/accelerate/pipelinec/kotlin/repository/" + it.key + "Repository.kt").writeText(it.value)
-        }
-    }
-
-    @Test
-    fun test_writeEntityDataClass_java() {
-        test_writeJavaEnums()
-
-        val resourceName = "sample/example.yaml"
-
-        val classLoader = javaClass.classLoader
-        val file = File(classLoader.getResource(resourceName)!!.file)
-
-        val yamlMap = parseYaml(file.absolutePath)
-
-        val entityClassRepresentations = writeEntityDataClassJava(yamlMap?.get("entities") as Map<String, Map<String, Any>>, yamlMap["packagePath"] as String)
-
-        entityClassRepresentations.forEach{
-            File("src/test/kotlin/wld/accelerate/pipelinec/java/entity/" + it.key +".java").writeText(it.value)
         }
     }
 
@@ -168,23 +95,6 @@ class YamlKtTest() {
         }
     }
 
-    @Test
-    fun test_writeModelClassJava() {
-        test_writeJavaEnums()
-
-        val resourceName = "sample/example.yaml"
-
-        val classLoader = javaClass.classLoader
-        val file = File(classLoader.getResource(resourceName)!!.file)
-
-        val yamlMap = parseYaml(file.absolutePath)
-
-        val modelClassRepresentations = writeJavaModelDataClass(yamlMap?.get("entities") as Map<String, Map<String, Any>>, yamlMap["packagePath"] as String)
-
-        modelClassRepresentations.forEach{
-            File("src/test/kotlin/wld/accelerate/pipelinec/java/model/" + it.key + "Model.java").writeText(it.value)
-        }
-    }
 
     @Test
     fun test_writeModelClass() {
@@ -201,22 +111,6 @@ class YamlKtTest() {
 
         modelClassRepresentations.forEach{
             File("src/test/kotlin/wld/accelerate/pipelinec/kotlin/model/" + it.key + "Model.kt").writeText(it.value)
-        }
-    }
-
-    @Test
-    fun test_writeServiceClass() {
-        val resourceName = "sample/example.yaml"
-
-        val classLoader = javaClass.classLoader
-        val file = File(classLoader.getResource(resourceName)!!.file)
-
-        val yamlMap = parseYaml(file.absolutePath)
-
-        val serviceClassRepresentations = writeJavaServiceClass(yamlMap?.get("entities") as Map<String, Map<String, Any>>, yamlMap["packagePath"] as String)
-
-        serviceClassRepresentations.forEach {
-            File("src/test/kotlin/wld/accelerate/pipelinec/java/service/" + it.key + "Service.java").writeText(it.value)
         }
     }
 
